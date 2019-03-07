7bf6176a2c67a5f17e294aacdd198d296ae3c375
Makungu Chansa <G17C9313@ict.ru.ac.za>
2019-03-07 15:46:35 +0200

    finished up until toTime function

--- Synthesis/Library.fs
+++ Synthesis/Library.fs
@@ -1,30 +1,47 @@
 ﻿module Synthesis
 
-let abelar _ =
+let abelar a =
+    (fun n -> 12 < n && n < 3097  && n % 12 =0) a
+   
+
+let area b h =
+   match b < 0.0 || h < 0.0 with 
+   | true -> failwith "Negative value given"
+   | _ -> b/2.0*h
+   
+    
+
+let zollo x =
+    match x < 0 with 
+    | true -> x * -1
+    | _ -> x * 2
+
+let min x y =
+    match x < y with 
+    | true -> x 
+    | _ -> y
+
+let max x y =
+    match x > y with 
+    | true -> x 
+    | _ -> y
+
+let ofTime h m s =
+    h*60*60 + m*60 + s
+
+let toTime t = 
+      let h = t/3600 in 
+      let m = (t-h*3600)/60 in
+      let s = t - (h*3600 + m*60) 
+      match t > 0 with 
+      | true -> h,m,s
+      | _ -> 0,0,0
+
+let digits n =
     failwith "Not implemented"
 
-let area _ _ =
-    failwith "Not implemented"
 
-let zollo _ =
-    failwith "Not implemented"
-
-let min _ _ =
-    failwith "Not implemented"
-
-let max _ _ =
-    failwith "Not implemented"
-
-let ofTime _ _ _ =
-    failwith "Not implemented"
-
-let toTime _ =
-    failwith "Not implemented"
-
-let digits _ =
-    failwith "Not implemented"
-
-let minmax _ =
+let minmax (a,b,c,d) =
     failwith "Not implemented"
 
 let isLeap _ =
